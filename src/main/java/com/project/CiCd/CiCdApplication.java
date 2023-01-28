d01b43c9fae3fc09eb7117a33057fef1228347e9
shev-81 <shev-81@mail.ru>
2023-01-28 23:09:00 +0300

    test pull

--- src/main/java/com/project/CiCd/CiCdApplication.java
+++ src/main/java/com/project/CiCd/CiCdApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * 
+ * Тестовый пулл
  */
 @SpringBootApplication
 public class CiCdApplication {
